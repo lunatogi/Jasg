37ab596c81f4c1d008b1621b771d7fde44b89b6d
Keti <ketomurat37@gmail.com>
2022-06-09 22:31:55 +0200

    Initial commit

---
+++ Assets/Scripts/GameplayScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayScript : MonoBehaviour {
+    public Transform spawnPoint;
+
+    Vector3 firstPos;
+    Vector3 lastPos;
+
+    float gapX;
+    float gapY;
+
+    public bool buttonDown = false;
+
+    GameObject ball;
+    public GameObject prefabBall;
+
+    public Transform[] gizmoPoints;
+
+    public float timer;
+	// Use this for initialization
+	void Start () {
+        ball = GameObject.FindGameObjectWithTag("ball");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1") && Input.mousePosition.y < Screen.height/2)
+        {
+            buttonDown = true;
+            firstPos = Input.mousePosition;
+        }else if (Input.GetButtonUp("Fire1") && buttonDown)
+        {
+            lastPos = Input.mousePosition;
+            gapX = lastPos.x - firstPos.x;
+            gapY = lastPos.y - firstPos.y;
+            float hipo = Mathf.Sqrt(gapX * gapX + gapY * gapY);
+            ball.GetComponent<CircleCollider2D>().isTrigger = true;
+            ball.GetComponent<Rigidbody2D>().velocity = new Vector3(gapX / hipo * 15, gapY / hipo * 15);
+            ball.gameObject.tag = "outball";
+            GameObject newBall = Instantiate(prefabBall, spawnPoint.position, spawnPoint.rotation);
+            newBall.gameObject.tag = "ball";
+            ball = GameObject.FindGameObjectWithTag("ball");
+            buttonDown = false;
+        }
+
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            GameObject[] targets = GameObject.FindGameObjectsWithTag("target");
+            foreach(GameObject target in targets)
+            {
+                target.SendMessage("SpeedUp", SendMessageOptions.DontRequireReceiver);
+            }
+            timer = 5;
+        }
+        
+	}
+}
---
+++ Assets/Scripts/TargetScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour {
+    public GameObject[] gizmos;
+    GameObject spawnPoint;
+    public bool loop = true;
+    public bool firstLoop = true;
+    public float speed = 1;
+
+    public float[] distances;
+    public int closest = 0;
+    public float closestDist = 10;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 4; i++)
+            gizmos[i] = GameObject.FindGameObjectWithTag("gizmo"+i);
+        spawnPoint = GameObject.FindGameObjectWithTag("spawn");
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (!loop) { 
+            transform.position = Vector2.MoveTowards(transform.position, spawnPoint.transform.position, Time.deltaTime);
+        }
+        else if (loop)
+        {
+            if (firstLoop)
+            {
+                SetLoop();
+                firstLoop = false;
+            }
+
+            if (closest == 3)
+            {
+                //transform.LookAt(gizmos[3].transform.position);
+                transform.position = Vector2.MoveTowards(transform.position, gizmos[0].transform.position, Time.deltaTime * speed);
+                if (transform.position == gizmos[0].transform.position)
+                    closest = 0;
+            }
+            else
+            {
+                //transform.LookAt(gizmos[closest - 1].transform.position);
+                transform.position = Vector2.MoveTowards(transform.position, gizmos[closest+1].transform.position, Time.deltaTime * speed);
+                if (transform.position == gizmos[closest+1].transform.position)
+                    closest++;
+            }
+        }
+	}
+
+    public void SetLoop()
+    {
+        for (int i = 0; i < gizmos.Length; i++)
+        {
+            distances[i] = Vector2.Distance(transform.position, gizmos[i].transform.position);
+            if (distances[i] < closestDist)
+            {
+                closest = i;
+                closestDist = distances[i];
+            }
+        }
+    }
+
+    public void SpeedUp()
+    {
+        speed += 0.2f;
+    }
+}
